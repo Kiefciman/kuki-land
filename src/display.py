1f92b5b18171fb5a371479d53e1edce6c14c12dc
Kiefciman <kiefciman@gmail.com>
2024-01-29 19:01:27 +0200

    a

--- src/display.py
+++ src/display.py
@@ -7,13 +7,14 @@ def display_init(self):
         vsync = vsync_on,
         #flags = pygame.OPENGL | pygame.FULLSCREEN
         #flags = pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE
-        flags = pygame.FULLSCREEN | pygame.DOUBLEBUF
+        #flags = pygame.FULLSCREEN | pygame.DOUBLEBUF
+        flags = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.FULLSCREEN
     )
     self.clock = pygame.time.Clock()
     pygame.display.set_caption('Kuki Land')
 
 def display_update(self):
-    dt = self.clock.tick() / 1000
+    dt = self.clock.tick(30) / 1000
     self.world.run(dt)
     pygame.display.update()
     #pygame.display.flip()
