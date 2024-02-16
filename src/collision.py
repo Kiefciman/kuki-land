5475151b087328c026c58bcf2b1d3c9957838c4e
Kiefciman <kiefciman@gmail.com>
2024-02-16 15:03:31 +0200

    a

--- src/collision.py
+++
@@ -1,7 +0,0 @@
-def collision(entity, collided_object):
-    if entity.moving_up == True:
-        if entity.hitbox.colliderect(collided_object.hitbox):
-            entity.can_go_up = False
-            entity.rect.bottom = collided_object.hitbox.bottom + 20
-        else:
-            entity.can_go_up = True
--- src/layers.py
+++
@@ -1,8 +0,0 @@
-layers = {
-    'water': 0,
-    'ground': 1,
-    'behind-player': 2,
-    'player': 3,
-    'infront-player': 4,
-    'z-sort': 5,
-}
